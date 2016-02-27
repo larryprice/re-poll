ffba5f06626fabaa817a13ae786d59cb05c8bedf
Larry Price <larry.price.dev@gmail.com>
2016-02-27 18:51:22 -0500

    cors with authorization

--- web.js
+++ web.js
@@ -52,7 +52,7 @@ app.use(bodyParser.json());
 app.use(morgan('combined'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
