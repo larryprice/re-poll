97bf451470a790a2b8fac03d338cb57949a8755c
Larry Price <larry.price.dev@gmail.com>
2016-02-27 18:49:51 -0500

    cors

--- web.js
+++ web.js
@@ -50,6 +50,11 @@ db.once('open', function() {
 
 app.use(bodyParser.json());
 app.use(morgan('combined'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res) {
 	res.send('Hello, World!');
